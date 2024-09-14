cc8b76971379c4e3175b1da1ae1f9ec3bfa9ca67
shivansh2501 <sbansal.official@gmail.com>
2024-09-14 16:08:14 +0530

    Second Class

---
+++ Lecture-01/Datatypes.java
@@ -0,0 +1,9 @@
+public class Datatypes {
+    public static void main (String[] args) {
+        //Primitive Datatypes
+        int a = 20;
+
+        System.out.print(a);
+        System.out.println(a);
+    }
+}
