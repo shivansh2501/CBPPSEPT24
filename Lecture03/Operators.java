6a0d69ac8f8301bcbcfd2c0e4c60fb5c838b7f26
shivansh2501 <sbansal.official@gmail.com>
2024-09-15 13:45:38 +0530

    Third Lecture

---
+++ Lecture03/Operators.java
@@ -0,0 +1,34 @@
+package Lecture03;
+
+public class Operators {
+    public static void main (String[] args) {
+        // int a = 10;
+        // int b = 12;
+        // int x = 8;
+
+        // if(a > b || a==b)
+        //     System.out.println("A is Greater");
+        // else
+        //     System.out.println("B is Greater");
+
+        // if (!(x > 5 && x < 10)) {
+        //     System.out.println( x + " is in between 5 and 10.");
+        // } else {
+        //     System.out.println(x + " is not in between 5 and 10.");
+        // }
+
+        //Assignment Operators
+        // int a = 5;
+        // int b = 10;
+
+        // b+=a;
+        // System.out.println(b);
+
+        //Bitwise Operators
+        int a = 9;
+        int b = 7;
+
+        System.out.println(a^b);
+    }
+    
+}
