aee1065b18c2de4ef4784194d50e932d1a1538ff
shivansh2501 <sbansal.official@gmail.com>
2024-09-21 23:08:27 +0530

    Completing Homework Questions

--- Lecture04/PatternPrinting2.java
+++ Lecture04/PatternPrinting2.java
@@ -71,6 +71,103 @@ public class PatternPrinting2 {
         }
     }
 
+    static void question13(int n) {
+        
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i = n - 1 ; i >= 1 ; i--) {
+            for(int j = 1 ; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void question14 (int n) {
+
+        //Printing upper triangle
+        for(int i = 1 ; i <= n ; i++) {
+
+            for(int k = 1 ; k <= n - i ; k++) {
+                System.out.print(" ");
+            }
+
+            for(int j = 1 ; j <= i ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for(int i = n - 1 ; i >= 1 ; i--) {
+
+            //Prining Spaces
+            for(int j = 1 ; j <= n - i ; j++) {
+                System.out.print(" ");
+            }
+
+            //Printing Stars
+            for(int k = 1 ; k <= i ; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void question10(int n) {
+
+        for(int i = n ; i >= 1 ; i--) {
+
+            for(int l = 1 ; l <= n - i ; l++) {
+                System.out.print("  ");
+            }
+
+            for(int j = 1 ; j <= i ; j++) {
+                System.out.print("* ");
+            }
+
+            for(int k = 2 ; k <= i ; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void question11 (int n) {
+
+        for(int i = 1 ; i <= n ; i++) {
+
+            for(int j = 1 ; j <= n - i ; j++) {
+                System.out.print("  ");
+            }
+
+            for(int k = 1 ; k <= i ; k++) {
+                if(k % 2 == 0) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+
+            for(int l = 1 ; l <= i ; l++) {
+                if((l % 2 ==0 && i % 2 == 0) || (l % 2 != 0 && i % 2 != 0)) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
     public static void main (String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -83,7 +180,7 @@ public class PatternPrinting2 {
 
         // printSquare(n);
 
-        printRFT(n);
+        question11(n);
 
         sc.close();
     }
