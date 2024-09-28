636270bca39edf57aaa4d55ba8bcd9d77735f4ea
shivansh2501 <sbansal.official@gmail.com>
2024-09-28 14:25:57 +0530

    Lecture- 6 in Progress

---
+++ Lecture06/PracticeArray.java
@@ -0,0 +1,83 @@
+package Lecture06;
+import java.util.*;
+
+public class PracticeArray {
+
+    static void display(int[] arr) {
+
+        for(int i = 0 ; i < arr.length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    static void reverse(int [] arr) {
+
+        for(int i = arr.length - 1 ; i >= 0 ; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
+
+    static void reverseProp(int[] arr) {
+
+        // Create a new array
+        int newArr[] = new int[arr.length];
+
+        // Add elements from old array in new array in reverse order
+        for(int i = 0, j = arr.length - 1 ; i <newArr.length ; i++, j--) {
+            newArr[i] = arr[j];
+        }
+
+        // Copy the new reversed array to the old array
+        for(int i = 0 ; i < arr.length ; i++) {
+            arr[i] = newArr[i];
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        // int[] arr = {1, 2, 3, 4, 5};
+
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // int[] arr = new int[n];
+
+        // for(int i = 0 ; i < arr.length ; i++) {
+        //     arr[i] = sc.nextInt();
+        // }
+
+        // System.out.println("Enter the index of elements to be swapped");
+
+        // int i = sc.nextInt();
+        // int j = sc.nextInt();
+
+        // swap(arr, i, j);
+
+        int[] arr = {1,2,3,4,5};
+
+        display(arr);
+
+        reverseProp(arr);
+
+        display(arr);
+
+        // display(arr);
+
+        //for-each loop
+        // for(int i : arr) {
+        //     System.out.print(i + " ");
+        // }
+
+        sc.close();
+    }
+    
+}
