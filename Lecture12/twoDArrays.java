dca9777823c1fa79b922c140dc4601bf138e9de1
shivansh2501 <sbansal.official@gmail.com>
2024-11-03 15:49:33 +0530

    Completed Lecture12

--- Lecture12/twoDArrays.java
+++ Lecture12/twoDArrays.java
@@ -19,31 +19,112 @@ public class twoDArrays {
 
         populateArray(arr, m, n);
 
-        print2DArray(arr, m, n);
+        print2DArray(arr);
 
         System.out.println();
 
         System.out.println(SumMatrix(arr, m, n));
 
+        // rowWiseSort(arr);
+
+        // print2DArray(arr, m, n);
+
+        // System.out.println();
+
+        // colWiseSort(arr);
+
+        System.out.println();
+
+        // sortAll(arr);
+
+        // print2DArray(arr, m, n);
+
+        // print2DArray(transpose(arr));
+
+        buildArray(arr);
+
         sc.close();
+    }
+
+    static void buildArray(int[][] arr) {
+
+        int rows = arr.length;
+        int cols = arr[0].length;
+
+        int[] oneDArray = new int[rows * cols];
+
+        for(int i = 0 ; i < arr.length ; i++) {
+            for(int j = 0 ; j < arr[0].length ; j++) {
+                oneDArray[cols*i + j] = arr[i][j];
+            }
+        }
+
+        for(int i = 0 ; i < oneDArray.length ; i++) {
+            System.out.print(oneDArray[i] + " ");
+        }
+
+        System.out.println();
+
+
+    }
+
+    static void fillArray(int[] arr, int[][] arr1) {
+
+        for(int i = 0 ; i < arr1.length ; i++) {
+            for(int j = 0 ; j < arr1[0].length ; j++) {
+                arr1[i][j] = arr[arr1[0].length * i + j];
+            }
+        }
+
+        //Print the 2D Array
+        for(int i = 0 ; i < arr1.length ; i++) {
+            for(int j = 0 ; j < arr1[0].length ; j++) {
+                System.out.print(arr1[i][j] + " ");
+            }
+        }
+
+        System.out.println();
+
     }
 
     static void populateArray(int[][] arr, int m, int n) {
 
-        int naturalNum = 1;
+        // int naturalNum = 1;
+
+        Random rand = new Random();
 
         for(int i = 0 ; i < m ; i++) {
             for(int j = 0 ; j < n ; j++) {
-                arr[i][j] = naturalNum++;
+                arr[i][j] = rand.nextInt(100);
             }
         }
 
     }
 
-    static void print2DArray(int[][] arr, int m, int n) {
+    static void rowWiseSort(int[][] arr) {
 
-        for(int i = 0 ; i < m ; i++) {
-            for(int j = 0 ; j < n ; j++) {
+        for(int i = 0 ; i < arr.length ; i++) {
+            Arrays.sort(arr[i]);
+        }
+    }
+
+    static void colWiseSort(int[][] arr) {
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[1], b[1]));
+
+    }
+
+    // static void sortAll(int[][] arr) {
+
+    //     rowWiseSort(arr);
+    //     colWiseSort(arr);
+
+    // }
+
+    static void print2DArray(int[][] arr) {
+
+        for(int i = 0 ; i < arr.length ; i++) {
+            for(int j = 0 ; j < arr[0].length ; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
@@ -62,6 +143,20 @@ public class twoDArrays {
         }
 
         return sum;
+
+    }
+
+    static int[][] transpose(int[][] arr) {
+
+        int trans[][] = new int[arr[0].length][arr.length];
+
+        for(int i = 0 ; i < arr.length ; i++) {
+            for(int j = 0 ; j < arr[0].length ; j++) {
+                trans[j][i] = arr[i][j];
+            }
+        }
+
+        return trans;
 
     }
     
