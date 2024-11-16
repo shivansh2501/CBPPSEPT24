76213377e05499e686976b56c16cb591a375daab
shivansh2501 <sbansal.official@gmail.com>
2024-11-17 00:32:05 +0530

    Lecture- 14 Completed

--- Lecture14/Strings.java
+++ Lecture14/Strings.java
@@ -32,7 +32,7 @@ public class Strings {
 
         String name = "Shivansh";
 
-        System.out.println(reverse(name));
+        System.out.println(firstOcc(name, 3));
 
 
     }
@@ -50,6 +50,41 @@ public class Strings {
 
         return result;
 
+    }
+
+    //Sliding window - HW
+
+    // static int compare(String s1, String s2) {
+
+    //     if(s1==s2) return 0;
+
+    //     for(int i = 0 ; i < s1.length() ; i++) {
+    //         if(s1.charAt(i) != s2.charAt(i))
+    //             return s1.charAt(i)-s2.charAt(i);
+    //     }
+
+    //     return s1.length()-s2.length();
+    // }
+
+    static String firstOcc(String s, int k) {
+
+        String ans = s.substring(0,k);
+
+        int i = 1;
+        k++;
+
+        while(k <= s.length()) {
+            String temp = s.substring(i, k);
+
+            if(ans.compareTo(temp) > 0) {
+                ans = temp;
+            }
+            i++;
+            k++;
+        }
+
+
+        return ans;
     }
 
 }
