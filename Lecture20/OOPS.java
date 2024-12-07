0ad8be269afa08f803c576daa2fd4da03b244027
shivansh2501 <sbansal.official@gmail.com>
2024-12-07 14:05:06 +0530

    In between Lecture- 20

--- Lecture20/OOPS.java
+++ Lecture20/OOPS.java
@@ -1,11 +1,19 @@
 package Lecture20;
-import java.util.*;
+// import java.util.*;
 
 public class OOPS {
     
     public static void main(String[] args) {
 
-        Classroom sept24 = new Classroom();
+        // Classroom sept24 = new Classroom();
+
+        Instructor inst1 = new Instructor("Shivansh", "Web Development", 88);
+
+        System.out.println(inst1.code + " " + inst1.name + " " + inst1.subject);
+
+        Student stud1 = new Student("Shivansh Bansal","CBPPSETP24",03,"Maharaja Agrasen Institute of Technology", 9.02);
+
+        System.out.println(stud1.name + " " + stud1.batch + " " + stud1.rollNum + " " + stud1.college + " " + stud1.CGPA);
 
     }
 
@@ -27,16 +35,25 @@ class Classroom {
 
 class Instructor {
 
-    //Constructor
-    public Instructor() {
-
-        
-
-    }
-
     String name;
     int code;
     String subject;
+
+       //constructor
+    public Instructor() {
+
+
+
+    }
+   
+       //Custom Constructor
+    public Instructor(String n, String s, int c) {
+   
+        this.name = n;
+        this.code = c;
+        this.subject = s;       
+   
+    }
 
 }
 
@@ -46,6 +63,27 @@ class Student {
     String batch;
     int rollNum;
     String college;
-    float CGPA;
+    double CGPA;
+
+    public Student() {
+
+        System.out.println("Creating student class default constructor");
+        this.name = "None";
+        this.batch = "Awara";
+        this.rollNum = -1;
+        this.college = "NA";
+        this.CGPA = -1;
+
+    }
+
+    public Student(String name, String batch, int rollNum, String college, double CGPA) {
+
+        this.name = name;
+        this.batch = batch;
+        this.rollNum = rollNum;
+        this.college = college;
+        this.CGPA = CGPA;
+
+    }
 
 }
